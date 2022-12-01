a2eece367b31585b7da9faf018eec4bdf68c4d24
FrancuzAlexandr <francuz_s@ukr.net>
2022-12-01 09:27:16 +0200

    task 1,2

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,21 @@
+import Profile from './profile/Profile';
+import Statistics from './statistics/Statistics';
+import user from '../data/user.json';
+import data from '../data/data.json';
+
 export const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
+    <div>
+      <Profile
+        username={user.username}
+        tag={user.tag}
+        location={user.location}
+        avatar={user.avatar}
+        followers={user.stats.followers}
+        views={user.stats.views}
+        likes={user.stats.likes}
+      />
+      <Statistics stats={data} title="Upload stats" />
     </div>
   );
 };
